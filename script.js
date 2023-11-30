a48ba4d6ea714a4fd593ff0f85570d1641ccef88
sabin <sabinsteel100@gmail.com>
2023-11-30 08:38:43 +0530

    assignment

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Responsive navigation menu
+{
+  let icon=document.getElementsByClassName("icon");
+  let para=document.getElementById("p1");
+  let para1=document.getElementById("p2");
+  let para2=document.getElementById("p3");
+  icon[0].addEventListener("click",function(){
+      para.textContent="Log in";
+      para1.textContent="Contact";
+      para2.textContent="About";
+  })
+  icon[0].addEventListener('click', () => {
+    document.getElementsByClassName('icon').classList.add('closed');
+  });
+}
+
+
 // change text on button click
 {
 let btn = document.getElementById('btn');
@@ -5,6 +22,7 @@ btn.addEventListener('click', function () {
     document.getElementById('para').innerHTML = "To infinity and beyond";
   });
 }
+
 
 //  creating a to-do list
 {
@@ -20,22 +38,42 @@ btn.addEventListener('click', function () {
   }
 }
 
-// form validation
+
+//image slideshow
 {
-  let name =document.getElementById("name");
-  name.addEventListener("keydown",function(){
-      alert("check name");
-  })
 
-  let email =document.getElementById("email");
-  email.addEventListener("keydown",function(){
-      alert("Input email contains both @ and .com ");
-  })
-
-  let passw =document.getElementById("passw");
-  passw.addEventListener("keydown",function(){
-      alert("At least 12 characters long but 14 or more is better");
-  })
-
-}
+  let img1 = document.getElementById('img1');
+  let img2 = document.getElementById('img2');
+  let img3 = document.getElementById('img3');
+  let img4 = document.getElementById('img4');
+  
+  let images = [img1, img2, img3, img4];
+  
+  let i = 0;
+  
+  let left = document.getElementById('left-arrow');
+  left.addEventListener('click', function(event) {
+      event.preventDefault()
+      if (i > 0) {
+          i = i - 1;
+          updateZIndex();
+      }
+  });
+  
+  let right = document.getElementById('right-arrow');
+  right.addEventListener('click', function(event) {
+      event.preventDefault()
+      if (i < images.length - 1) {
+          i = i + 1;
+          updateZIndex();
+      }
+  });
+  
+  function updateZIndex() {
+      for (let j = 0; j < images.length; j++) {
+          images[j].style.zIndex = (j === i) ? 4 : 3;
+      }
+  }
+  
+  }
 
