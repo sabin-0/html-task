dc42814086b4c2b2fde9a32e1dfde317018a103e
sabin <sabinsteel100@gmail.com>
2023-12-13 11:31:45 +0530

    javascript

--- script.js
+++ script.js
@@ -27,36 +27,63 @@ setTimeout( ()=> {
 // let datas = fetch('https://jsonplaceholder.typicode.com/users');
 // console.log("datas : ", datas);
 
+console.log("Third line...");
+
 //types of requests
 //get 
 //post
 //put
 //delete
 
-console.log("Third line...");
-
 
 console.log("\n\n\nAjax..");
 
 let xhr = new XMLHttpRequest();
+let btn = document.getElementById('btn');
 
 xhr.open("get",'https://jsonplaceholder.typicode.com/users');
 
-xhr.send();
+btn.addEventListener('click',()=>{
+  xhr.send();  
+})
+
 
 console.log("xhr response : ",xhr);
 
 xhr.onreadystatechange = function () {
     console.log("readyState : ",xhr.readyState);
 
+    let content = document.getElementById('content');
+    console.log("content : ",content);
+    let datas = '';
+
     if(xhr.readyState == 4) {
+
+        if(xhr.status == 200) {
         console.log("response : ",xhr.response);
         console.log("type of response : ",typeof(xhr.response));
 
         let parsed_response = JSON.parse(xhr.response);
         console.log("parsed_response : ",parsed_response);
         console.log("type of parsed_response : ",typeof(parsed_response));
+
+        for(let i=0; i<parsed_response.length; i++) {
+            datas = datas + `<tr>
+            <td>${parsed_response[i].id}</td>
+            <td>${parsed_response[i].name}</td>
+            <td>${parsed_response[i].username}</td>
+            <td>${parsed_response[i].website}</td>
+            </tr>`;
+        }
+
+        content.innerHTML = datas;
+    }else {
+        console.log("Request failed");
     }
+}else {
+    console.log("State not completed");
+}
+
 }
 
 
