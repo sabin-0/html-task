178e7c76308277e405419c0f6ed1b2c77ec489e8
sabin <sabinsteel100@gmail.com>
2024-01-10 11:25:32 +0530

    aa

--- client/script.js
+++ client/script.js
@@ -16,10 +16,11 @@ async function getData() {
         rows = rows + `
         <tr>
         <td>${parsedData[i]._id}</td>
-        <td>${parsedData[i].firstname}</td>
-        <td>${parsedData[i].lastname}</td>
-        <td>${parsedData[i].email}</td>
-        <td>${parsedData[i].password}</td>
+        <td><input type="text" name="firstname" id="firstname-${parsedData[i]._id}" value=${parsedData[i].firstname}" disabled=true></td>
+        <td><input type="text" name="lastname" id="lastname-${parsedData[i]._id}" value="${parsedData[i].lastname}" disabled=true></td>
+        <td><input type="email" name="email" id="email-${parsedData[i]._id}" value="${parsedData[i].email}" disabled=true></td>
+        <td><input type="password" name="password" id="password-${parsedData[i]._id}" value="${parsedData[i].password}" disabled=true></td>
+        <td><button onclick="handleEdit('${parsedData[i]._id}')">Edit</button></td>
         
         `
     }
@@ -28,3 +29,23 @@ async function getData() {
 }
 
 getData();
+
+function handleEdit(id) {
+    console.log("id :", id);
+
+    let firstname = document.getElementById(`firstname-${id}`);
+    console.log("firstname : ", firstname);
+    firstname.disabled = false;
+
+    let lastname = document.getElementById(`lastname-${id}`);
+    console.log("lastname : ", lastname);
+    lastname.disabled = false;
+
+    let email = document.getElementById(`email-${id}`);
+    console.log("email : ", email);
+    email.disabled = false;
+
+    let password = document.getElementById(`password-${id}`);
+    console.log("password : ", password);
+    password.disabled = false;
+}
