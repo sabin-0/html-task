8a30dcb370d08bb87da77cbceaa1f6680805338c
sabin <sabnsteel100@gmail.com>
2024-01-18 11:27:56 +0530

    express2

--- server.js
+++ server.js
@@ -4,22 +4,32 @@ const { MongoClient } = require('mongodb');
 const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT;
+
 const client =new MongoClient("mongodb://localhost:27017");
 const db= client.db('ums');
 const collection = db.collection("users");
 
-console.log("__dirname : ", __dirname);
-app.use('/',express.static(__dirname + '/client'));
-app.use(express.urlencoded({extended : false}));
-app.use(express.json());//to parse json datas
-
-app.get('/test',(req,res)=>{
-    res.status(200).send("success");
+//Test route
+app.get('/test',(req,res, next)=> {
+  // res.status(200).send("success");
+  next();
+},(req,res, next) => {
+  //console.log("Succes1");
+  //res,status(200).send("Success1");
+  next();
+}, (req,res) => {
+  console.log("Success2");
+  res.status(200).send("Success2");
 });
 
+console.log("__dirname : ", __dirname);
+app.use('/',express.static(__dirname + '/client'));
+app.use(express.urlencoded({extended : false}));//To parse form datas
+app.use(express.json());//to parse json datas
+
 app.post('/submit',async (req,res)=>{
-    console.log("reached here");
-    console.log("req body : ",req.body);
+    let data = req.body;
+    console.log("data : ", data);
 
     await collection.insertOne(req.body)
         .then((message=>{
@@ -32,18 +42,44 @@ app.post('/submit',async (req,res)=>{
         })
 });
 
-app.get('/getData',async (req,res)=>{
-    const formData = collection.find();
-    console.log("formData : ", formData);
+app.get('/getData', async (req, res) => {
+  let data = await collection.find().toArray();
+  console.log("data : " , data);
 
-    const formDataArr = await formData.toArray();
-    console.log("formDataArr : ", formDataArr);
+  let json_data = JSON.stringify(data);
+  console.log("json_data : ", json_data);
 
-    let jsonFormData = JSON.stringify(formDataArr);
-    console.log("jsonFormData : ", jsonFormData);
+  res.status(200).send(json_data);
+});
 
-   res.status(200).send(jsonFormData);
-})
+app.put('/editData', async (req, res) => {
+  let data = req.body;
+  console.log("data : ", data);
+
+  let id = data.id;
+  console.log("id : ", id);
+  console.log("typeof(id) : ", typeof (id));
+  let _id = new ObjectId(id);
+  console.log("_id : ", _id);
+  console.log("typeof(_id) : ", typeof (_id));
+
+  let updateDatas = {
+    name : data.name,
+    email : data.email,
+    password : data.password,
+
+  }
+
+  await collection.updateOne({_id},{$set : updateDatas})
+  .then((message)=>{
+    console.log("Document updated successfully : ", message);
+    res.status(200).send("success");
+  })
+  .catch((error)=>{
+    console.log("Document not updated : ", error);
+    res.status(400).send("failed");
+  })
+});
 
 async function connect() {
 
